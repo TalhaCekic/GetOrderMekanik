a1ab1a8497286786b0b1b0a79a749ff95747ed54
Myso <talhacekicc123@gmail.com>
2023-07-16 18:00:00 +0300

    a

--- mechanicalTesting/Assets/ESKIHaL/script/itemBase/pickUp.cs
+++ mechanicalTesting/Assets/ESKIHaL/script/itemBase/pickUp.cs
@@ -60,7 +60,7 @@ public class pickUp : NetworkBehaviour
     public static bool cutting = false;
 
 
- //   public NetworkVariable<controller>  playerInput = new NetworkVariable<controller>();
+    public NetworkVariable<controller>  playerInput = new NetworkVariable<controller>();
 
     public RigBuilder rigBuilder;
     public GameObject objectToRemove; // Kaldýrýlacak objeyi atan deðiþken
@@ -69,7 +69,7 @@ public class pickUp : NetworkBehaviour
     {
         if (!IsOwner) return;
         rigBuilder = GetComponent<RigBuilder>();
-      //  playerInput.Value = new controller();
+        playerInput.Value = new controller();
         gameController = GameObject.FindGameObjectWithTag("GameController");
         Counter = FindObjectOfType<counter>();
 
@@ -82,10 +82,10 @@ public class pickUp : NetworkBehaviour
     void Update()
     {
         if (!IsOwner) return;
-        //playerInput.Value.Player.Interact.performed += x => Interact();
-        //playerInput.Value.Player.Drop.performed += x => Drop();
+        playerInput.Value.Player.Interact.performed += x => Interact();
+        playerInput.Value.Player.Drop.performed += x => Drop();
 
-        //playerInput.Value.Player.CuttingWash.performed += PressCuttingAndWashing;
+        playerInput.Value.Player.CuttingWash.performed += PressCuttingAndWashing;
 
         IDcheck();
 
