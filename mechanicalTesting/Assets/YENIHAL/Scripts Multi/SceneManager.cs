f92af6557240e7cbea4399dd601c70a2a83efc85
Myso <talhacekicc123@gmail.com>
2023-07-19 17:52:08 +0300

    a

--- mechanicalTesting/Assets/YENIHAL/Scripts Multi/SceneManager.cs
+++ mechanicalTesting/Assets/YENIHAL/Scripts Multi/SceneManager.cs
@@ -8,49 +8,49 @@ using TMPro;
 
 public class SceneManager : NetworkBehaviour
 {
-    [SyncVar]
-    public int playerCount;
-    // Start is called before the first frame update
-    protected Callback<LobbyEnter_t> LobbyEntered;
-    public ulong CurrentLobbyID;
+    //[SyncVar]
+    //public int playerCount;
+    //// Start is called before the first frame update
+    //protected Callback<LobbyEnter_t> LobbyEntered;
+    //public ulong CurrentLobbyID;
 
-    private CustomNetworkManager manager;
-    private const string HostAddressKey = "HostAddress";
-    private void Start()
-    {
-        if (!SteamManager.Initialized) { return; }
-        manager = GetComponent<CustomNetworkManager>();
-        LobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
-    }
-
-    //public void OnLobbyEntered(LobbyEnter_t pCallback)
+    //private CustomNetworkManager manager;
+    //private const string HostAddressKey = "HostAddress";
+    //private void Start()
     //{
-
-    //    CSteamID steamId = SteamUser.GetSteamID();
-    //    playerCount = SteamMatchmaking.GetNumLobbyMembers((CSteamID)pCallback.m_ulSteamIDLobby);
-    //    Debug.Log("Player joined. Current players in lobby: " + playerCount);
+    //    if (!SteamManager.Initialized) { return; }
+    //    manager = GetComponent<CustomNetworkManager>();
+    //    LobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
     //}
-    private void OnTriggerStay(Collider other)
-    {
-        print(playerCount);
-        //if (playerCount == 1)
-        //{
-        //    print("aaaaaa");
-        //}
-    }
 
-    private void OnLobbyEntered(LobbyEnter_t callback)
-    {
+    ////public void OnLobbyEntered(LobbyEnter_t pCallback)
+    ////{
+
+    ////    CSteamID steamId = SteamUser.GetSteamID();
+    ////    playerCount = SteamMatchmaking.GetNumLobbyMembers((CSteamID)pCallback.m_ulSteamIDLobby);
+    ////    Debug.Log("Player joined. Current players in lobby: " + playerCount);
+    ////}
+    //private void OnTriggerStay(Collider other)
+    //{
+    //    print(playerCount);
+    //    //if (playerCount == 1)
+    //    //{
+    //    //    print("aaaaaa");
+    //    //}
+    //}
+
+    //private void OnLobbyEntered(LobbyEnter_t callback)
+    //{
         
-        CurrentLobbyID = callback.m_ulSteamIDLobby;
+    //    CurrentLobbyID = callback.m_ulSteamIDLobby;
         
-        playerCount = SteamMatchmaking.GetNumLobbyMembers((CSteamID)callback.m_ulSteamIDLobby);
+    //    playerCount = SteamMatchmaking.GetNumLobbyMembers((CSteamID)callback.m_ulSteamIDLobby);
 
-        if (NetworkServer.active) { return; }
+    //    if (NetworkServer.active) { return; }
 
-        manager.networkAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
+    //    manager.networkAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
 
-        manager.StartClient();
+    //    manager.StartClient();
 
-    }
+    //}
 }
