9cdc7774c9e2f9db2bb7306626e03e6c5c3ef111
Myso <talhacekicc123@gmail.com>
2023-07-21 11:12:55 +0300

    a

--- mechanicalTesting/Assets/ESKIHaL/script/itemBase/pickUp.cs
+++ mechanicalTesting/Assets/ESKIHaL/script/itemBase/pickUp.cs
@@ -63,7 +63,6 @@ public class pickUp : NetworkBehaviour
     }
     void Start()
     {
-        if (!isLocalPlayer) return;
         playerInput = new controller();
 
         pickupLayerMask = LayerMask.GetMask("Pickup");
