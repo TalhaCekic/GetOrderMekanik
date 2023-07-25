3387c1ae480914f930b730a39f06c5cfebfada7b
canberk yolcu <c.yolcu99@gmail.com>
2023-07-25 23:22:46 +0300

    a

--- mechanicalTesting/Assets/ESKIHaL/script/itemBase/pickUp.cs
+++ mechanicalTesting/Assets/ESKIHaL/script/itemBase/pickUp.cs
@@ -2867,11 +2867,11 @@ public class pickUp : NetworkBehaviour
 
     public void Interact()
     {
-        //if (!NetworkClient.ready)
-        //{
-        //    NetworkClient.Ready();
-        //}
-        if (full && isLocalPlayer) { IDcheck(); }
+        if (!NetworkClient.ready)
+        {
+            NetworkClient.Ready();
+        }
+        if (full) { IDcheck(); }
         //  counter;
         if (Physics.Raycast(this.transform.position, transform.TransformDirection(Vector3.forward), out hit, hitRange, pickupLayerMask2))
         {
