a21ae895d13bbb3f3f767b725b315b632d21d2a8
Myso <talhacekicc123@gmail.com>
2023-08-12 19:25:52 +0300

    mekanik clone

---
+++ GetOrderM_clone_0/Assets/Scripts/SceneManager1.cs
@@ -0,0 +1,106 @@
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steamworks;
+using System;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneManager1 : NetworkBehaviour
+{
+
+    [SyncVar]
+    private float timeCounter = 0f;
+
+    [SyncVar] public int LobbyCount;
+
+    public GameObject[] playerClone;
+
+    private bool test = false;
+
+    public PlayerListManager playerListManager;
+
+    //UI
+    public Slider stayGround;
+
+    private void Start()
+    {
+        timeCounter = 0;
+    }
+    private void Update()
+    {
+        if (test)
+        {
+            timeCounter += Time.deltaTime;
+            stayGround.value = timeCounter;
+            if (timeCounter >= 1)
+            {
+                SceneManager.LoadScene(2);
+                NetworkManager.singleton.ServerChangeScene("PcScene");
+
+            }
+        }
+
+
+
+     //   Debug.Log(timeCounter);
+    }
+
+    public void OpenFriendsList()
+    {
+        if (!SteamManager.Initialized) { return; }
+
+        // Steam overlay'ini kullanarak arkadaþ listesini açar
+        SteamFriends.ActivateGameOverlay("Friends");
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            LobbyCount += 1;
+
+            if (LobbyCount == playerListManager.playerCount && isServer)
+            {
+                test = true;
+             
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            LobbyCount -= 1;
+            test = false;
+            timeCounter = 0;
+            //  if(LobbyCount!=playerListManager.playerCount)
+            //StopCoroutine(TimeCounter());
+
+        }
+    }
+
+
+    //IEnumerator TimeCounter()
+    //{
+    //    yield return new WaitForSeconds(0.5f); 
+    //    timeCounter -= Time.deltaTime;
+    //    if (LobbyCount == playerListManager.playerCount)
+    //    {
+
+    //        if (timeCounter < 0)
+    //        {
+    //            SceneManager.LoadScene(2);
+    //            NetworkManager.singleton.ServerChangeScene("PcScene");
+    //        }
+
+    //        //SceneManager.MoveGameObjectToScene(playerClone[LobbyCount], scene);
+    //    }
+    //}
+}
---
+++ GetOrderM_clone_0/Assets/Scripts/player/itemID.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class itemID : MonoBehaviour 
+{
+
+    public float ItemID;
+    public string Name;
+}
