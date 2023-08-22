192adaaa5ba06a844382fa95d5c0109d869ebec6
canberk yolcu <c.yolcu99@gmail.com>
2023-08-22 21:18:44 +0300

    a

--- GetOrderM/Assets/Scripts/MultiPlayer/UILobbyName.cs
+++ GetOrderM/Assets/Scripts/MultiPlayer/UILobbyName.cs
@@ -12,10 +12,10 @@ public class UILobbyName : NetworkBehaviour
     //public readonly SyncList<PlayerGenerete> playerGenerete = new SyncList<PlayerGenerete>();
 
     [SerializeField] private RawImage[] rawImage;
-    //public readonly SyncList<RawImage> rawImage = new SyncList<RawImage>();
+    
 
     [SerializeField] private Color[] color;
-    //public readonly SyncList<Color> color = new SyncList<Color>();
+  
 
     [SyncVar]
     private int playerCount;
@@ -27,11 +27,12 @@ public class UILobbyName : NetworkBehaviour
         {
             rawImage[i].gameObject.SetActive(false);
         }
+        
     }
     private void Update()
     {
         playerCount = NetworkManager.singleton.numPlayers;
-
+       // CmdUILobbyNames(rawImage,playerGenerete,color);
         //if (isServer)
         //{
         //    RpcUILobbyNames();
@@ -42,38 +43,39 @@ public class UILobbyName : NetworkBehaviour
         //}
 
     }
-    [Command(requiresAuthority = false)]
-    public void CmdUILobbyNames()
-    {
-        RpcUILobbyNames();
-    }
+    //[Command(requiresAuthority = false)]
+    //public void CmdUILobbyNames(RawImage[] raw, PlayerGenerete[] player, Color[] color1)
+    //{
+    //    RpcUILobbyNames(raw,player,color1);
+    //}
 
-    [ClientRpc]
-    public void RpcUILobbyNames()
-    {
-        print("test");
-        for (int i = 0; i < playerCount; i++)
-        {
-            color[i] = playerGenerete[i].playerColor;
-            rawImage[i].color = color[i];
-            rawImage[i].gameObject.SetActive(true);
-        }
-    }
-    public void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
+    //[ClientRpc]
+    //public void RpcUILobbyNames(RawImage[] raw, PlayerGenerete[] player, Color[] color1)
+    //{
+    //    print("test");
+    //    for (int i = 0; i < playerCount; i++)
+    //    {
+    //        player[i] = FindObjectOfType<PlayerGenerete>();
+    //        color1[i] = player[i].playerColor;
+    //        raw[i].color = color1[i];
+    //        raw[i].gameObject.SetActive(true);
+    //    }
+    //}
+    //public void OnTriggerStay(Collider other)
+    //{
+    //    if (other.gameObject.CompareTag("Player"))
+    //    {
            
-            for (int i = 0; i < playerCount; i++)
-            {
-                playerGenerete[i] = FindObjectOfType<PlayerGenerete>();
-                color[i] = playerGenerete[i].playerColor;
-                rawImage[i].color = color[i];
-                rawImage[i].gameObject.SetActive(true);
-            }
+    //        for (int i = 0; i < playerCount; i++)
+    //        {
+    //            playerGenerete[i] = FindObjectOfType<PlayerGenerete>();
+    //            color[i] = playerGenerete[i].playerColor;
+    //            rawImage[i].color = color[i];
+    //            rawImage[i].gameObject.SetActive(true);
+    //        }
 
-        }
-    }
+    //    }
+    //}
 
 
 }
