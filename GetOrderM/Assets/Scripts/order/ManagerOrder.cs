32b6f535b0c3b9617c65c4f0924df45b82a34aea
canberk yolcu <c.yolcu99@gmail.com>
2023-09-12 16:36:22 +0300

    Update ManagerOrder.cs

--- GetOrderM/Assets/Scripts/order/ManagerOrder.cs
+++ GetOrderM/Assets/Scripts/order/ManagerOrder.cs
@@ -25,7 +25,7 @@ public class ManagerOrder : NetworkBehaviour
     
     private void Start()
     {
-       // DontDestroyOnLoad(this);
+        DontDestroyOnLoad(this);
         CalculateNextOrderTime();
     }
     private void Update()
