d9e25dc1a692a77d9d0095626450b7c47d1fcbd8
canberk yolcu <c.yolcu99@gmail.com>
2023-09-13 00:20:06 +0300

    Update OrderTimes.cs

--- GetOrderM/Assets/Scripts/order/OrderTimes.cs
+++ GetOrderM/Assets/Scripts/order/OrderTimes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-public class OrderTimes : MonoBehaviour
+public class OrderTimes : NetworkBehaviour
 {
      public  float currentCouldown;
 }
