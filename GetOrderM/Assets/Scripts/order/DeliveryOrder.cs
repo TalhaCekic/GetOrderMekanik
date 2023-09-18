5f0c551825a8f2ec01235f0b397fcb6d41ac0703
Myso <talhacekicc123@gmail.com>
2023-09-18 23:42:59 +0300

    Update DeliveryOrder.cs

--- GetOrderM/Assets/Scripts/order/DeliveryOrder.cs
+++ GetOrderM/Assets/Scripts/order/DeliveryOrder.cs
@@ -51,10 +51,6 @@ public class DeliveryOrder : NetworkBehaviour
         {
             rpc(currentobjectnumber);
         }
-
-
-
-
     }
     [Command(requiresAuthority = false)]
     public void AddObjectToList(GameObject obj)
