54b636617d8475c10f17fcee01761064a0a6556f
Myso <talhacekicc123@gmail.com>
2023-09-18 23:31:52 +0300

    Update DeliveryOrder.cs

--- GetOrderM/Assets/Scripts/order/DeliveryOrder.cs
+++ GetOrderM/Assets/Scripts/order/DeliveryOrder.cs
@@ -46,7 +46,8 @@ public class DeliveryOrder : NetworkBehaviour
     {
        
             cmdIdCheck(currentobjectnumber);
-            cmd(currentobjectnumber);
+        //cmd(currentobjectnumber);
+        rpc(currentobjectnumber);
         
 
 
