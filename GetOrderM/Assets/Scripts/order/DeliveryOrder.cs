03e91f47cc4e32233b47e6062eafd75484474e7d
canberk yolcu <c.yolcu99@gmail.com>
2023-09-19 14:18:34 +0300

    Update DeliveryOrder.cs

--- GetOrderM/Assets/Scripts/order/DeliveryOrder.cs
+++ GetOrderM/Assets/Scripts/order/DeliveryOrder.cs
@@ -517,12 +517,12 @@ public class DeliveryOrder : NetworkBehaviour
             {
                 if (managerOrder.orderArray[i] == submidID)
                 {
+                    managerOrder.orderArray[i] = currentID;
                     currentobjectnumber = 0;
                     orderUI[i].GetComponent<OrderTimes>().currentCouldown = 0;
-                    orderUI.Remove(orderUI[i]);
+                   // orderUI.Remove(orderUI[i]);
                     lastResetTime = Time.time;
                     orderCorrect = true;
-                    managerOrder.orderArray[i] = currentID;
                     break;
                 }
                 else
