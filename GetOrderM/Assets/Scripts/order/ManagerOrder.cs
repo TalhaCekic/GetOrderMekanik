e9ed9e7bf0faeaa68e5607a36d77772f8442cfaf
canberk yolcu <c.yolcu99@gmail.com>
2023-09-22 01:32:32 +0300

    Update ManagerOrder.cs

--- GetOrderM/Assets/Scripts/order/ManagerOrder.cs
+++ GetOrderM/Assets/Scripts/order/ManagerOrder.cs
@@ -262,7 +262,7 @@ public class ManagerOrder : NetworkBehaviour
         for (int i = 0; i < orderUI.Count; i++)
         {
             orderUI[i].gameObject.transform.position = parentTransform[i].transform.position;
-            orderUI[i].transform.parent = canvas.transform.parent;
+           // orderUI[i].transform.parent = canvas.transform.parent;
         }
     }
 }
