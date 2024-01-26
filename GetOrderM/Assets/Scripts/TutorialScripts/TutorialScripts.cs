562c16ddc40cf292d20a9e39a9e51a7fdb64e357
Myso <talhacekicc123@gmail.com>
2024-01-26 16:50:16 +0300

    a

--- GetOrderM/Assets/Scripts/TutorialScripts/TutorialScripts.cs
+++ GetOrderM/Assets/Scripts/TutorialScripts/TutorialScripts.cs
@@ -11,9 +11,14 @@ using Steamworks;
 public class TutorialScripts : NetworkBehaviour
 {
     private int currentStep = 0;
-    private string[] tutorialSteps = { "'E' Tuþuna basarak bilgisayar kasasýný al"," 'E' Tuþuna basarak bilgisayar kasasýný masaya býrak", "'Q' tuþuna basarak bilgisayar kasasýný kutusunu aç ", "'E' Tuþuna basarak anakartý  al",
-    "'E' Tuþuna basarak anakartý masaya býrak","'Q' Tuþuna basarak anakart kutusunu aç","'E' Tuþuna basarak anakartý al", "'E' Tuþuna basarak anakartý kasanýn içine koy", "'E' Tuþuna basarak kasayý al",
-        "'E' Tuþuna basarak sipariþi teslim et", "Tebrikler artýk oyunu oynamaya hazýrsýn. 'p' Tuþuna basarak ana menüye dön."  };
+    private string[] tutorialStepsTr = { "'E' Tu?una basarak bilgisayar kasas?n? al", "'E' Tu?una basarak bilgisayar kasas?n? masaya b?rak", "'Q' tu?una basarak bilgisayar kasas?n? kutusunu aç", "'E' Tu?una basarak anakart? al",
+        "'E' Tu?una basarak anakart? masaya b?rak", "'Q' Tu?una basarak anakart kutusunu aç", "'E' Tu?una basarak anakart? al", "'E' Tu?una basarak anakart? kasan?n içine koy", "'E' Tu?una basarak kasay? al",
+        "'E' Tu?una basarak sipari?i teslim et", "Tebrikler art?k oyunu oynamaya haz?rs?n. 'p' Tu?una basarak ana menüye dön." };
+    private string[] tutorialStepsEng = { "Press 'E' to pick up the computer case", "Press 'E' to place the computer case on the table", "Press 'Q' to open the computer case", "Press 'E' to pick up the motherboard",
+        "Press 'E' to place the motherboard on the table", "Press 'Q' to open the motherboard box", "Press 'E' to pick up the motherboard", "Press 'E' to place the motherboard into the case", "Press 'E' to pick up the case",
+        "Press 'E' to deliver the order", "Congratulations, you are now ready to play the game. Press 'p' to return to the main menu." };
+
+
     private string[] requiredInputs = { "e", "e", "q", "e", "e", "q", "e", "e", "e", "e","e" };
     private bool[] stepCompleted;
     public TextMeshProUGUI text;
@@ -23,19 +28,19 @@ public class TutorialScripts : NetworkBehaviour
 
     private void Start()
     {
-        text.text = tutorialSteps[currentStep];
+        text.text = tutorialStepsEng[currentStep];
         SetActiveTutorialImage(currentStep);
     }
 
     private void Update()
     {
-        if (currentStep < tutorialSteps.Length)
+        if (currentStep < tutorialStepsEng.Length)
         {
             CheckInput();
         }
         else
         {
-            Debug.Log("Tutorial tamamlandi. ?yi i?!");
+            //Debug.Log("Tutorial tamamlandi. ?yi i?!");
         }
 
         MenuyeDon();
@@ -67,9 +72,9 @@ public class TutorialScripts : NetworkBehaviour
             Debug.Log("Tebrikler! Adým " + (currentStep + 1) + " tamamlandý.");
             SetInactiveTutorialImage(currentStep);
             currentStep++;
-            if (currentStep < tutorialSteps.Length)
+            if (currentStep < tutorialStepsEng.Length)
             {
-                text.text = tutorialSteps[currentStep];
+                text.text = tutorialStepsEng[currentStep];
                 SetActiveTutorialImage(currentStep);
             }
         }
